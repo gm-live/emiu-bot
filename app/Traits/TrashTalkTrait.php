17facd414af5f9c5229d240c0efba7bc66716451
wright <wright.huang@baiyitech.com.tw>
2021-11-02 19:53:31 +0800

    feat: add trash talk (I am real)

--- app/Traits/TrashTalkTrait.php
+++ app/Traits/TrashTalkTrait.php
@@ -6,31 +6,31 @@ namespace App\Traits;
 
 trait TrashTalkTrait
 {
-    protected $iEmiuUserID = 1382889010;
+    protected $iEmiuUserID   = 1382889010;
     protected $iPaiPaiUserID = 1330462756;
 
     protected $iElevenFloorChatId = -1001301971976;
     // protected $iElevenFloorChatId = -718317972;
 
-	public function beefNoodle($aMessage): void
-	{
-		$iChatId    = $aMessage['chat']['id'];
+    public function beefNoodle($aMessage): void
+    {
+        $iChatId    = $aMessage['chat']['id'];
         $sText      = $aMessage['text'] ?? '';
         $iMessageId = $aMessage['message_id'];
-		if (
-			(
+        if (
+            (
                 strpos($sText, 'www.ubereats.com') === false &&
-    			strpos($sText, 'eats.uber.com') === false
+                strpos($sText, 'eats.uber.com') === false
             ) ||
             strpos($sText, '牛肉麵') === false
-		) {
+        ) {
             return;
         }
 
         $sTagString = $this->getTagUserString($this->iEmiuUserID, 'Emiu');
-        $sMsg = "{$sTagString} \!  牛肉麵吃起來\!";
+        $sMsg       = "{$sTagString} \!  牛肉麵吃起來\!";
         $this->sendMsg($iChatId, $sMsg, $iMessageId);
-	}
+    }
 
     public function braisedPorkOnRice($aMessage): void
     {
@@ -54,20 +54,20 @@ trait TrashTalkTrait
         }
 
         $sTagString = $this->getTagUserString($this->iEmiuUserID, 'Emiu');
-        $sMsg = "{$sTagString} \!  魯肉飯吃起來\!";
+        $sMsg       = "{$sTagString} \!  魯肉飯吃起來\!";
         $this->sendMsg($iChatId, $sMsg, $iMessageId);
     }
 
-	public function paipai($aMessage): void
-	{
+    public function paipai($aMessage): void
+    {
         $iMessageId = $aMessage['message_id'];
-        $iUserId  = $aMessage['from']['id'];
-        
+        $iUserId    = $aMessage['from']['id'];
+
         if ($iUserId != $this->iPaiPaiUserID) {
-        	return;
+            return;
         }
 
-		$iChatId = $aMessage['chat']['id'];
+        $iChatId = $aMessage['chat']['id'];
         $sText   = $aMessage['text'] ?? '';
 
         if (strpos($sText, '@' . config('bot.username')) === false) {
@@ -75,27 +75,41 @@ trait TrashTalkTrait
         }
 
         $this->sendSticker(
-        	$iChatId, 
-        	'CAACAgUAAxkBAAICGWFv9Z-duPPt6P4wEx50avKwH5L4AAICAAORWGIqT2SuySGu6v8hBA',
+            $iChatId,
+            'CAACAgUAAxkBAAICGWFv9Z-duPPt6P4wEx50avKwH5L4AAICAAORWGIqT2SuySGu6v8hBA',
             $iMessageId,
         );
 
         $sTagString = $this->getTagUserString($this->iPaiPaiUserID, '派派哥');
-        $sMsg = $sTagString . '！ 甘蔗！';
+        $sMsg       = $sTagString . '！ 甘蔗！';
         $this->sendMsg($iChatId, $sMsg, $iMessageId);
-	}
+    }
+
+    public function iAmReal($aMessage)
+    {
+        $sText      = $aMessage['text'] ?? '';
+        $iMessageId = $aMessage['message_id'];
+        $iUserId    = $aMessage['from']['id'];
+        $iChatId    = $aMessage['chat']['id'];
+        if (
+            $this->iEmiuUserID == $iUserId && 
+            ($sText == '你是假的' || $sText == '你是假的')
+        ) {
+            $this->sendMsg($iChatId, '你才假\! 你全家都假\!', $iMessageId);
+        }
+    }
 
     public function emiuGoOffWork()
     {
         $sTagString = $this->getTagUserString($this->iEmiuUserID, 'Emiu');
-        $sMsg = "本 {$sTagString} 要下班了\!";
+        $sMsg       = "本 {$sTagString} 要下班了\!";
         $this->sendMsg($this->iElevenFloorChatId, $sMsg);
     }
 
     public function emiuOrderMeal()
     {
         $sTagString = $this->getTagUserString($this->iEmiuUserID, 'Emiu');
-        $sMsg = "大{$sTagString}~~ 該訂晚餐了\!";
+        $sMsg       = "大{$sTagString}~~ 該訂晚餐了\!";
         $this->sendMsg($this->iElevenFloorChatId, $sMsg);
     }
 
--- config/autoload/bot.php
+++ config/autoload/bot.php
@@ -44,5 +44,6 @@ return [
         'beefNoodle',
         'braisedPorkOnRice',
         'paipai',
+        'iAmReal',
     ],
 ];
