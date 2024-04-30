c041c446ea8e21c8a27c92cdc57559c688514d35
wright <wright.huang@baiyitech.com.tw>
2024-04-30 19:35:29 +0900

    feat: change port

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -1,6 +1,6 @@
 <?php
 
-declare (strict_types = 1);
+declare(strict_types=1);
 /**
  * This file is part of Hyperf.
  *
@@ -14,34 +14,34 @@ use Hyperf\Server\Server;
 use Swoole\Constant;
 
 return [
-    'mode'      => SWOOLE_PROCESS,
-    'servers'   => [
+    'mode' => SWOOLE_PROCESS,
+    'servers' => [
         [
-            'name'      => 'http',
-            'type'      => Server::SERVER_HTTP,
-            'host'      => '0.0.0.0',
-            'port'      => 11001,
+            'name' => 'http',
+            'type' => Server::SERVER_HTTP,
+            'host' => '0.0.0.0',
+            'port' => 8082,
             'sock_type' => SWOOLE_SOCK_TCP,
             'callbacks' => [
                 Event::ON_MANAGER_START => [App\ServerListener\OnManagerStartListener::class, 'handle'],
-                Event::ON_REQUEST       => [Hyperf\HttpServer\Server::class, 'onRequest'],
+                Event::ON_REQUEST => [Hyperf\HttpServer\Server::class, 'onRequest'],
             ],
         ],
     ],
-    'settings'  => [
-        Constant::OPTION_ENABLE_COROUTINE      => true,
-        Constant::OPTION_WORKER_NUM            => swoole_cpu_num(),
-        Constant::OPTION_PID_FILE              => BASE_PATH . '/runtime/hyperf.pid',
-        Constant::OPTION_OPEN_TCP_NODELAY      => true,
-        Constant::OPTION_MAX_COROUTINE         => 100000,
-        Constant::OPTION_OPEN_HTTP2_PROTOCOL   => true,
-        Constant::OPTION_MAX_REQUEST           => 100000,
-        Constant::OPTION_SOCKET_BUFFER_SIZE    => 2 * 1024 * 1024,
-        Constant::OPTION_BUFFER_OUTPUT_SIZE    => 2 * 1024 * 1024,
+    'settings' => [
+        Constant::OPTION_ENABLE_COROUTINE => true,
+        Constant::OPTION_WORKER_NUM => swoole_cpu_num(),
+        Constant::OPTION_PID_FILE => BASE_PATH . '/runtime/hyperf.pid',
+        Constant::OPTION_OPEN_TCP_NODELAY => true,
+        Constant::OPTION_MAX_COROUTINE => 100000,
+        Constant::OPTION_OPEN_HTTP2_PROTOCOL => true,
+        Constant::OPTION_MAX_REQUEST => 100000,
+        Constant::OPTION_SOCKET_BUFFER_SIZE => 2 * 1024 * 1024,
+        Constant::OPTION_BUFFER_OUTPUT_SIZE => 2 * 1024 * 1024,
     ],
     'callbacks' => [
         Event::ON_WORKER_START => [Hyperf\Framework\Bootstrap\WorkerStartCallback::class, 'onWorkerStart'],
         Event::ON_PIPE_MESSAGE => [Hyperf\Framework\Bootstrap\PipeMessageCallback::class, 'onPipeMessage'],
-        Event::ON_WORKER_EXIT  => [Hyperf\Framework\Bootstrap\WorkerExitCallback::class, 'onWorkerExit'],
+        Event::ON_WORKER_EXIT => [Hyperf\Framework\Bootstrap\WorkerExitCallback::class, 'onWorkerExit'],
     ],
 ];
